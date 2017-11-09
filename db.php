6243878257040507754573f99e89b528fa258c67
Damar <damarjokowi@gmail.com>
2017-11-10 00:24:56 +0700

    087- Retrieving data from MySQL

--- db.php
+++ db.php
@@ -9,16 +9,36 @@ if(mysqli_connect_errno()){
 	);
 }
 ?>
+<?php
+$query  = "SELECT * ";
+$query .= "FROM subjects ";
+$query .= "WHERE visible = 1 ";
+$query .= "ORDER BY position ASC";
+
+$result = mysqli_query($connection, $query);
+if (!result){
+die("Database query failed.");
+}
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
 <head>
-<title>untitled</title>
+<title>Database</title>
 </head>
 <body>
 
+<?php
+while($row = mysqli_fetch_row($row)){
+var_dump($row);
+echo "<hr />";
+}
+?>
+<?php
+mysqli_free_result($result);
+?>
 </body>
 </html>
 
